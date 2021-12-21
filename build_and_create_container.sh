f2822e9d390f6ffd64de63565057bc4685027c93
Lukas Seitz <lukas-sinzing@gmx.de>
2021-12-21 18:06:25 +0100

    automaticly copys torrc file to /docker/tor_relay

--- build_and_create_container.sh
+++ build_and_create_container.sh
@@ -2,8 +2,16 @@ docker build \
     -t tor_image \
     .
 
+# create dir
+mkdir /docker/tor_relay/
+
+#copy torrc
+cp torrc /docker/tor_relay/
+
 docker run \
+    -d \
     --name "tor_relay" \
     --publish 9001:9001 \
     --publish 9030:9030 \
-    -v /docker/tor_relay:/etc/tor/
+    -v /docker/tor_relay:/etc/tor/ \
+    tor_image
